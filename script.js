7ee83abb34cbe126a1e6b9b07653eb9edfce719b
Vivek-Tiwari <127580268+VivekTiwari2003@users.noreply.github.com>
2023-08-09 13:00:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,18 @@
 //your JS code here. If required.
+
+const getWeatherBtn = document.getElementById("getWeatherBtn");
+const weatherDataDiv = document.getElementById("weatherData");
+
+getWeatherBtn.addEventListener("click", () => {
+    const apiUrl = "https://api.openweathermap.org/data/2.5/weather?q=London&appid=YOUR_API_KEY_HERE";
+    
+    fetch(apiUrl)
+        .then(response => response.json())
+        .then(data => {
+            const weatherDescription = data.weather[0].description;
+            weatherDataDiv.textContent = `Current weather in London: ${weatherDescription}`;
+        })
+        .catch(error => {
+            console.error("Error fetching weather data:", error);
+        });
+});
